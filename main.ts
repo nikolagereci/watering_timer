06be0db206f4fbd2af03d9e89af03e0898462e16
nikolagereci <37083905+nikolagereci@users.noreply.github.com>
2022-06-02 13:09:57 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,28 @@
+input.onButtonPressed(Button.A, function () {
+    if (interval > 1) {
+        interval += 0 - 1
+    }
+    basic.showString("I" + interval)
+})
+
+input.onButtonPressed(Button.B, function () {
+    interval += 1
+    basic.showString("I" + interval)
+})
+
+function water(watering_period: number) {
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    basic.pause(watering_period * MINUTE)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+}
+
+let interval = 1
+let watering_period = 1
+let MINUTE = 60000
+let HOUR = MINUTE * 60
+led.setBrightness(9)
+
+basic.forever(function () {
+    basic.pause(interval * HOUR)
+    water(watering_period)
+})
